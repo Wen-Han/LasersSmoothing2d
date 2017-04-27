88fe918b0f51fc68e68837587372eb675d054f47
Wen-Han <hanwen.omega@gmail.com>
2017-04-26 17:42:05 -0700

    Add more smoothing methods.
    Now support 'FM SSD', 'GS RPM SSD', 'AR RPM SSD', 'GS ISI', 'AR ISI'.
    All smoothing method use the same definition of bandwidth.
    Add an output option to save the plots as png files. Non-interactive output is done in parallel (shared-memory-style).

--- LaserSmoothing2d.py
+++ LaserSmoothing2d.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
+from multiprocessing import Pool
 
 # focal length of the final lens, in meter
 focal_length = 7.7
@@ -10,39 +11,55 @@ beam_aperture = 0.35
 wave_length = 0.351e-9
 # number of beamlets
 n_beams = [64, 64]
-# type of smoothing. valid options are 'FM SSD', 'RPM SSD', 'ISI'
-lsType = 'FM SSD'
+# grid points in each direction
+n_grid = [128, 128]
+# types of smoothing. valid options are:
+# 'FM SSD', 'GS RPM SSD', 'AR RPM SSD', 'GS ISI', 'AR ISI'
+lsType = 'AR RPM SSD'
+# if apply simple average to AR(1)
+if_sma = True
 # number of color cycles
-ncc = [12.2, 12.2]
+ncc = [1.0, 1.0]
 # (RMS value of) the amplitude of phase modulation
 beta = 4
-if beta <= 0.0:
-    # static speckle pattern. all models are reduced to phase plate
-    nuTotal = 0.0
-    nu = 0.0
-else:
-    # bandwidth of the optics, normalized to laser frequency
-    nuTotal = 0.001 / np.sqrt(2)
-    # bandwidth of phase modulation, normalized to laser frequency
-    nu = nuTotal / 2 / beta
+# bandwidth of the optics, normalized to laser frequency
+nuTotal = 0.00117
 # electric field amplitude of each beamlet, scalar or 1d numpy array
 e0 = 1.0
 # complex transform for each beamlet, scalar or 1d numpy array
 epsilon_n = 1.0
-# length of the movie, normalized to 1/laser frequency. (# of laser cycle)
-tMaxMovie = 1e4
-# length of the time series, normalized to 1/laser frequency. (# of laser cycle)
-tMax = 2e2 + tMaxMovie
+# length of the movie, normalized to 1/omega0.
+tMaxMovie = 2.2e3
 # time delay imposed by one echelon step in ISI, in 1/nuTotal
 tDelay = 1.5
-# delta time for the movie, in 1/laser frequency, the code will round it so as
+# delta time for the movie, in 1/omega_0, the code will round it so as
 # to complete tMax with integer steps. Increasing dt can reduce calculation
-# time. Does not apply to FM SSD.
-dt = 100.0
+# time. Does not apply to FM SSD in interactive plot
+dt = 200.0
+# interactive plot or saving to files
+interactive_plot = False
 # ------------------------------------------------------------------------------
+# input ends
 
 # XDL unit
 xdl = wave_length / beam_aperture
+
+ncc = np.array(ncc)
+lsType = lsType.upper()
+# length of the time series, normalized to 1/omega0.
+tMax = dt + tMaxMovie
+
+if 'SSD' in lsType:
+    nuTotal /= np.sqrt(2)
+    if beta > 0:
+        nu = 0.5 * nuTotal / beta
+    else:
+        nu = 0
+    # s is the parameter for gratings in SSD. equal to time delay in xdl units
+    if nu > 0:
+        s = np.divide(2 * np.pi * ncc, nu)
+    else:
+        s = [0.0, 0.0]
 
 
 def general_form_beamlets_2d(amp, trans_n, psi_n, ps_n):
@@ -60,10 +77,25 @@ def general_form_beamlets_2d(amp, trans_n, psi_n, ps_n):
 
 
 # phase plate doesn't change over time
-phi_n = np.pi * np.random.binomial(1, 0.5, (n_beams[0], n_beams[1]))
+# RPP
+# phi_n = np.pi * np.random.binomial(1, 0.5, (n_beams[0], n_beams[1]))
+# CPP
+phi_n = np.pi * np.random.uniform(-np.pi, np.pi, (n_beams[0], n_beams[1]))
 # x0, x1 are normalized to the beam aperture
 x0, x1 = np.meshgrid(np.linspace(-0.5, 0.5, num=n_beams[0]),
                      np.linspace(-0.5, 0.5, num=n_beams[1]))
+
+
+def sma1d(pha, num):
+    ret = np.cumsum(pha)
+    ret[num:] = ret[num:] - ret[:-num]
+    return ret / num
+
+
+def ar1(b, sigma, pha, num=1):
+    """ Autoregressive process of order 1 
+    """
+    return b * pha + np.random.normal(scale=sigma, size=num)
 
 
 def ssd_2d_fm(t):
@@ -72,16 +104,40 @@ def ssd_2d_fm(t):
     :param t: current time
     :return: near field electric field amplitude of the full beam
     """
-    psi_n = beta * (np.sin(2 * np.pi * nu * (t + s[0] * x0)) +
-                    np.sin(2 * np.pi * nu * (t + s[1] * x1)))
+    psi_n = beta * (np.sin(nu * (t + s[0] * x0)) +
+                    np.sin(nu * (t + s[1] * x1)))
     return general_form_beamlets_2d(e0, epsilon_n, psi_n, phi_n)
 
+# time delay array for beamlets
+tn_d = np.arange(0.0, n_beams[0] * n_beams[1]).reshape(n_beams)
+tn = np.long(0)
 
-# s is the parameter for gratings in SSD. equal to time delay in xdl units
-if nu > 0:
-    s = np.divide(ncc, nu)
-else:
-    s = [0.0, 0.0]
+
+def sma_ar1(ttn, tot_bandwidth, pm_am):
+    n = 0
+    if if_sma:
+        # generate a longer series to avoid edge effect of SMA
+        n = int(1 / (tot_bandwidth / pm_am ** 2) / dt)
+        ttn += 2 * n
+    # AR1 phase
+    random_phase = np.zeros(ttn)
+    # PSD of AR1 process is Lorentzian
+    pm_bw = 0.5 * tot_bandwidth / (pm_am * pm_am)
+    arcoeff1 = np.exp(- dt * pm_bw)
+    arcoeff2 = np.sqrt(1 - arcoeff1 * arcoeff1) * pm_am
+    # Discard the first part of the random sequence
+    for ti in range(256):
+        random_phase[0] = ar1(arcoeff1, arcoeff2, random_phase[0])
+    for ti in range(1, ttn):
+        random_phase[ti] = ar1(arcoeff1, arcoeff2, random_phase[ti - 1])
+    if if_sma:
+        denor = (arcoeff1 * arcoeff1 * (n * arcoeff1 - n + 2) -
+                 2 * np.power(arcoeff1, n + 1) * (
+                     arcoeff1 - 1) + n - arcoeff1 * (n + 2))
+        nor = ((1 - arcoeff1 * arcoeff1) * np.square(1 - arcoeff1) * n * n)
+        var = np.sqrt(nor / denor)
+        random_phase = sma1d(random_phase, n) * var
+    return random_phase[n:ttn - n]
 
 
 def ssd_2d_rpm_init():
@@ -91,15 +147,29 @@ def ssd_2d_rpm_init():
     """
     # make sure tMax is larger than the time delay introduced by gratings
     global tMax
-    global s
+    global tn_d
+    global tn
+    global dt
     if nu > 0:
-        tMax += np.max(np.divide(ncc, nu))
-        s = np.divide(ncc, nu)
-    return gen_gaussian_time_series(np.long(tMax / dt), nu * 2, beta)
-
-
-tn_d = np.arange(0.0, n_beams[0] * n_beams[1]).reshape(n_beams)
-tn = np.long(0)
+        tMax += np.sum(s)
+        # s = np.divide(ncc, nu)
+    tn = np.long(tMax / dt)
+    dt = tMax / tn
+    tdx0, tdx1 = np.meshgrid(np.linspace(0.0, 1.0, num=n_beams[0]),
+                             np.linspace(0.0, 1.0, num=n_beams[1]))
+    tn_d = np.zeros((2, n_beams[0], n_beams[1]))
+    tn_d[0, :, :] = tdx0 * s[0] / dt
+    tn_d[1, :, :] = tdx1 * s[1] / dt
+    tn_d = tn_d.astype(long)
+    # random phases in x and y direction are independent
+    ph = np.zeros((2, tn))
+    if 'GS' in lsType:
+        ph[0, :] = gen_gaussian_time_series(tn, 0.5 * nuTotal / beta, beta)
+        ph[1, :] = gen_gaussian_time_series(tn, 0.5 * nuTotal / beta, beta)
+    else:
+        ph[0, :] = sma_ar1(tn, nuTotal, beta)
+        ph[1, :] = sma_ar1(tn, nuTotal, beta)
+    return ph
 
 
 def isi_2d_init():
@@ -109,13 +179,21 @@ def isi_2d_init():
     """
     global tMax
     global tn_d
+    global tn
+    global dt
+    # make sure we generate a long enough phase sequence
     if nuTotal > 0:
-        tMax += np.prod(n_beams) / nuTotal * tDelay
+        tMax += (np.prod(n_beams) + 1) / nuTotal * tDelay
         tn_d *= tDelay / nuTotal / dt
         tn_d = tn_d.astype(long)
+    tn = np.long(tMax / dt)
+    dt = tMax / tn
     # this may take a while ...
-    random_phase = gen_gaussian_time_series(np.long(tMax / dt / 2),
-                                            nuTotal / np.pi, np.pi)
+    if 'GS' in lsType:
+        random_phase = gen_gaussian_time_series(tn,
+                                                0.5 * nuTotal / beta, beta)
+    else:
+        random_phase = sma_ar1(tn, nuTotal, beta)
     return np.reshape(random_phase, random_phase.size)
 
 
@@ -125,25 +203,16 @@ def gen_gaussian_time_series(t_num, fwhm, rms_mean):
     :param t_num: number of grid points in time
     :param fwhm: full width half maximum of the power spectrum
     :param rms_mean: root-mean-square average of the spectrum
-    :return: a time series array with shape [2, t_num]
+    :return: a time series array with shape [t_num]
     """
-    global tn
-    global dt
-    tn = t_num
-    dt = tMax / tn
     if fwhm == 0.0:
-        return np.zeros((2, tn))
-    pm_phase = np.random.uniform(-0.5, 0.5, (2, tn))
-    spec = np.fft.fftshift(np.fft.fft(pm_phase), axes=-1)
+        return np.zeros((2, t_num))
     omega = np.fft.fftshift(np.fft.fftfreq(t_num, d=dt))
-    spec_filter = np.exp(- np.square(omega / fwhm * 2 *
-                                     np.sqrt(0.5 * np.log(2))))
-    spec[0, :] *= spec_filter
-    spec[1, :] *= spec_filter
-    time_seq_filtered = np.real(np.fft.ifft(np.fft.ifftshift(spec)))
-    rms = np.sqrt(np.mean(np.square(time_seq_filtered), axis=1))
-    pm_phase[0, :] = time_seq_filtered[0, :] / rms[0] * rms_mean
-    pm_phase[1, :] = time_seq_filtered[1, :] / rms[1] * rms_mean
+    rand_ph = np.random.normal(scale=np.pi, size=t_num)
+    psd = np.exp(-np.log(2) * 0.5 * np.square(omega / fwhm * 2 * np.pi))
+    psd *= np.sqrt(2 * t_num) / np.sqrt(np.mean(np.square(psd))) * rms_mean
+    pm_phase = np.array(psd) * (np.cos(rand_ph) + 1j * np.sin(rand_ph))
+    pm_phase = np.real(np.fft.ifft(np.fft.ifftshift(pm_phase)))
     return pm_phase
 
 
@@ -154,10 +223,12 @@ def ssd_2d_rpm(t):
     :return: near field electric field amplitude of the full beam
     """
     # dt = tMax / tn
-    indx = np.array([(t - s[0] * x0.flatten()) / dt,
-                     (t - s[1] * x1.flatten()) / dt])
-    indx = indx.astype(long)
-    psi_n = np.reshape(pmPhase[0, indx[0, :]] + pmPhase[1, indx[1, :]],
+    # indx = np.array([(t - s[0] * x0.flatten()) / dt,
+    #                  (t - s[1] * x1.flatten()) / dt])
+    tt = np.long(t / dt)
+    indx0 = tt + tn_d[0, :, :].flatten()
+    indx1 = tt + tn_d[1, :, :].flatten()
+    psi_n = np.reshape(pmPhase[0, indx0] + pmPhase[1, indx1],
                        n_beams)
     return general_form_beamlets_2d(e0, epsilon_n, psi_n, phi_n)
 
@@ -169,33 +240,41 @@ def isi_2d(t):
     :return: near field electric field amplitude of the full beam
     """
     tt = np.long(t / dt)
-    indx = np.array(tt - tn_d.flatten())
-    indx = np.mod(indx.astype(long), tn)
+    indx = np.array(tt + tn_d.flatten())
     psi_n = np.reshape(pmPhase[indx], n_beams)
     return general_form_beamlets_2d(e0, epsilon_n, psi_n, phi_n)
 
 
 func_dict = {
     'FM SSD': ssd_2d_fm,
-    'RPM SSD': ssd_2d_rpm,
-    'ISI': isi_2d,
+    'GS RPM SSD': ssd_2d_rpm,
+    'AR RPM SSD': ssd_2d_rpm,
+    'AR ISI': isi_2d,
+    'GS ISI': isi_2d,
 }
 
 
-def select_laser_smoothing_2d(pm_type='FM SSD'):
+def select_laser_smoothing_2d(pm='FM SSD'):
     """ select which smoothing technique to use
 
-    :param pm_type: a string denoting the type, defaulting to 'FM SSD'
+    :param pm: a string denoting the type, defaulting to 'FM SSD'
     :return: ssd function, ssd_2d_fm or ssd_2d_rpm
     """
-    if pm_type.upper() == 'RPM SSD':
+    if 'RPM SSD' in pm:
         pm_phase = ssd_2d_rpm_init()
-    elif pm_type.upper() == 'ISI':
+    elif 'ISI' in pm:
         pm_phase = isi_2d_init()
     else:
-        pm_type = 'FM SSD'
+        pm = 'FM SSD'
+        # No random phase needed for FM SSD
         pm_phase = None
-    return func_dict[pm_type.upper()], pm_phase
+    return func_dict[pm], pm_phase
+
+dx = np.divide(2 * np.pi, n_grid)
+xlp0, xlp1 = np.meshgrid(np.linspace(-0.5 * n_beams[0], 0.5 * n_beams[0],
+                                     num=n_beams[0]),
+                         np.linspace(-0.5 * n_beams[1], 0.5 * n_beams[1],
+                                     num=n_beams[1]))
 
 
 def focal_len_2d(beamlets):
@@ -204,60 +283,99 @@ def focal_len_2d(beamlets):
     :param beamlets: electric field of full beam
     :return: far fields pattern on the focal plane
     """
-    field = np.multiply(proPhase, np.fft.fftshift(np.fft.fft2(beamlets)))
+    field = np.zeros(n_grid, dtype=complex)
+    if n_beams[0] == n_grid[0] and n_beams[1] == n_grid[1]:
+        field = np.fft.fft2(beamlets)
+    else:
+        # naive sum to calculate the Fourier transform
+        for ibx in range(-n_grid[0] / 2, n_grid[0] - n_grid[0] / 2):
+            for iby in range(-n_grid[1] / 2, n_grid[1] - n_grid[1] / 2):
+                field[ibx, iby] = np.sum(np.multiply(
+                    np.exp(1j * (ibx * dx[0] * xlp0 + iby * dx[1] * xlp1)),
+                    beamlets))
+    field = np.multiply(proPhase, np.fft.fftshift(field))
     return field
+
+
+def plot_2d_xy(fld, tm):
+    """ save the absolute values of fld as a png file
+    """
+    plt.figure(figsize=(4.5, 4.5))
+    plt.imshow(np.abs(np.real(fld)), cmap='gray', aspect='equal',
+               extent=[-n_beams[0] / 2 + 0.5, n_beams[0] / 2 - 0.5,
+                       -n_beams[1] / 2 + 0.5, n_beams[1] / 2 - 0.5],
+               interpolation=inter_opt, vmin=0,
+               vmax=np.sqrt(9 * n_beams[0] * n_beams[1]))
+    plt.title('$E_{env}$ (' + lsType.upper() +
+              ', t='+"{0:.3f}".format(tm*dt*1.86e-4)+'ps)')
+    plt.xlabel('$x (\lambda f/D)$')
+    plt.ylabel('$y (\lambda f/D)$')
+    plt.savefig('test'+"{0:0>4}".format(tm)+'.png')
+    plt.close()
 
 time = 0
 laser_smoothing_2d, pmPhase = select_laser_smoothing_2d(lsType)
 
-axis_color = 'lightgoldenrodyellow'
-fig = plt.figure()
-fig.canvas.set_window_title(lsType)
-
-fig.add_subplot(121)
-# fig.subplots_adjust(left=0.1, bottom=0.25)
-bl = laser_smoothing_2d(time)
-nn = bl.shape
-im0 = plt.imshow(np.real(bl), cmap='gray', aspect='equal',
-                 extent=[-0.5, 0.5, -0.5, 0.5])
-# im0 = plt.imshow(np.square(np.real(bl)), cmap='gray', aspect='equal')
-plt.title('E before final focal lens')
-plt.xlabel('x\' ($D$)')
-plt.ylabel('y\' ($D$)')
-# bl_tmp = bl
-gn = bl.shape
+gn = n_grid
 xfp0, xfp1 = np.meshgrid(np.linspace(-0.5 * gn[0], 0.5 * gn[0], num=gn[0]),
                          np.linspace(-0.5 * gn[1], 0.5 * gn[1], num=gn[1]))
 # constant phase shift due to beam propagation
 proPhase = np.exp(1j * (np.square(xfp0) + np.square(xfp1))
                   * xdl * focal_length / beam_aperture * np.pi +
                   2j * np.pi * focal_length / wave_length)
+# setting up some plotting options
+inter_opt = 'sinc'
 
-ax = fig.add_subplot(122)
-fp_tmp = focal_len_2d(bl)
-im1 = plt.imshow(np.square(np.real(fp_tmp)), cmap='gray', aspect='equal',
-                 extent=[-n_beams[0] / 2 + 0.5, n_beams[0] / 2 - 0.5,
-                         -n_beams[1] / 2 + 0.5, n_beams[1] / 2 - 0.5])
-plt.title('Intensity near focal spot')
-plt.xlabel('$x (\lambda f/D)$')
-plt.ylabel('$y (\lambda f/D)$')
-fig.set_tight_layout(True)
-# fig.subplots_adjust(left=0.1, bottom=0.25)
+if interactive_plot:
+    bl = laser_smoothing_2d(time)
+    fp_tmp = focal_len_2d(bl)
+    axis_color = 'lightgoldenrodyellow'
+    fig = plt.figure()
+    fig.canvas.set_window_title(lsType)
 
-# Add sliders for tweaking the parameters
-time_slider_ax = fig.add_axes([0.2, 0.05, 0.65, 0.03], axisbg=axis_color)
-time_slider = Slider(time_slider_ax, 'Time (laser cycle)',
-                     0.0, tMaxMovie, valfmt='%1d', valinit=time)
+    fig.add_subplot(121)
+    im0 = plt.imshow(np.real(bl), cmap='gray', aspect='equal',
+                     extent=[-0.5, 0.5, -0.5, 0.5])
+    plt.title('E before final focal lens')
+    plt.xlabel('x\' ($D$)')
+    plt.ylabel('y\' ($D$)')
+    ax = fig.add_subplot(122)
+    im1 = plt.imshow(np.abs(np.real(fp_tmp)), cmap='gray', aspect='equal',
+                     extent=[-n_beams[0] / 2 + 0.5, n_beams[0] / 2 - 0.5,
+                             -n_beams[1] / 2 + 0.5, n_beams[1] / 2 - 0.5],
+                     interpolation=inter_opt, vmin=0,
+                     vmax=np.sqrt(9 * n_beams[0] * n_beams[1]))
+    plt.title('Intensity near focal spot')
+    plt.xlabel('$x (\lambda f/D)$')
+    plt.ylabel('$y (\lambda f/D)$')
+    fig.set_tight_layout(True)
+    # fig.subplots_adjust(left=0.1, bottom=0.25)
 
+    # Add sliders for tweaking the parameters
+    time_slider_ax = fig.add_axes([0.2, 0.05, 0.65, 0.03], axisbg=axis_color)
+    time_slider = Slider(time_slider_ax, 'Time (laser cycle)',
+                         0.0, tMaxMovie, valfmt='%1d', valinit=time)
 
-def sliders_on_changed(val):
-    bl_new = laser_smoothing_2d(time_slider.val)
-    im0.set_data(np.real(bl_new))
-    # im0.set_data(np.square(np.real(bl_new)))
-    tmp = focal_len_2d(bl_new)
-    im1.set_data(np.square(np.real(tmp)))
-    fig.canvas.draw_idle()
-time_slider.on_changed(sliders_on_changed)
+    def sliders_on_changed(val):
+        bl_new = laser_smoothing_2d(time_slider.val)
+        im0.set_data(np.real(bl_new))
+        # im0.set_data(np.square(np.real(bl_new)))
+        tmp = focal_len_2d(bl_new)
+        im1.set_data(np.abs(np.real(tmp)))
+        fig.canvas.draw_idle()
 
+    time_slider.on_changed(sliders_on_changed)
+    plt.show()
+else:
 
-plt.show()
+    def speckle_time_t(i):
+        speckle = laser_smoothing_2d(i * dt)
+        fp_speckle = focal_len_2d(speckle)
+        plot_2d_xy(fp_speckle, i)
+
+    tnMaxMovie = int(tMaxMovie / dt)
+    # the phase information have been known/constructed for any time t, each
+    # process can calculate different frames simultaneously/independently
+    if __name__ == '__main__':
+        pool = Pool()
+        pool.map(speckle_time_t, range(tnMaxMovie))
